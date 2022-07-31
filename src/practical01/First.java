9c2f0656bd1f41e179a63179440fc8c458ab1cb6
xKaimac <91130920+xKaimac@users.noreply.github.com>
2022-07-31 19:03:21 +0930

    Delete src/practical01 directory

--- src/practical01/First.java
+++
@@ -1,9 +0,0 @@
-package practical01;
-
-public class First {
-    public static void main(String[] args) {
-        System.out.println((7*6) + " = " + 7 + " * " + 6 + " (answer 1)");
-        System.out.println(3 + " + " + 5 + " = " + (3+5) + " (answer 2)");
-        System.out.println("The ideas of \"state\" and \"sequence\" are fundamental to most programming");
-    }
-}
